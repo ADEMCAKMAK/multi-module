a52f22979e6855e95c3c78da4bfd83871c60db09
adem.cakmak@etiya.com <adm.cakmak67@gmail.com>
2020-08-29 22:33:20 +0300

    empty lines

--- root/src/main/java/com/example/root/RootApplication.java
+++ root/src/main/java/com/example/root/RootApplication.java
@@ -15,6 +15,8 @@ public class RootApplication /*extends SpringBootServletInitializer */{
     }
 
 /*
+
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(RootApplication.class);
